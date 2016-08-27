87ed547f7a021e4cc4329dcc1dc26d571789a188
duhdanube <Danny.B.Lim@gmail.com>
2016-08-26 19:06:36 -0700

    ready for deployment

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.use('/search', require('./search'));
 
 module.exports = router;
---
+++ server/routes/search.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+router.route('/search/:query')
+  .get((req, res) => {
+
+  })
+
+
+module.exports = router;
