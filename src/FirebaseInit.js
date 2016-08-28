2e97b0dee9133e75834beafbe1767ac052357a05
duhdanube <Danny.B.Lim@gmail.com>
2016-08-27 23:30:21 -0700

    deployment attempt 3

--- src/FirebaseInit.js
+++ src/FirebaseInit.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyA8QsFkU8YwJ-zw13p2I-xTpTGKPp4HZ7A",
+    apiKey: process.env.API_KEY,
     authDomain: "tamil-nadu-seoul.firebaseapp.com",
     databaseURL: "https://tamil-nadu-seoul.firebaseio.com",
     storageBucket: "tamil-nadu-seoul.appspot.com",
